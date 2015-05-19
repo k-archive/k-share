josephg/ShareJS

Don't send subscribes for queries and docs
Should make reconnects MUCH more efficient

lib/client/query.js
@@ -63,12 +63,9 @@ Query.prototype._execute = function() {
     // don't need to be sent their snapshots again.
     for (var i = 0; i < this.knownDocs.length; i++) {
       var doc = this.knownDocs[i];
-      // If we're subscribed, the server already knows which version of the doc
-      // we have.
-      if (!doc.subscribed && doc.action !== 'subscribe') {
-        var c = collectionVersions[doc.collection] = collectionVersions[doc.collection] || {};
-        c[doc.name] = doc.version;
-      }
+      var c = collectionVersions[doc.collection] =
+        (collectionVersions[doc.collection] || {});
+      c[doc.name] = doc.version;
     }
   }
 
@@ -95,7 +92,6 @@ Query.prototype._execute = function() {
 Query.prototype._dataToDocs = function(data) {
   var results = [];
   var lastType;
-  this.connection.bsStart();
   for (var i = 0; i < data.length; i++) {
     var docData = data[i];
 
@@ -107,20 +103,23 @@ Query.prototype._dataToDocs = function(data) {
     }
 
     var doc = this.connection.get(docData.c || this.collection, docData.d, docData);
-    // Force the document to know its subscribed if we're in docmode:subscribe.
+    // Force the document to know its subscribed if we're in docmode: 'sub'
     if (this.docMode === 'sub') {
       if (docData.v > doc.version) {
+        // This generally shouldn't happen, but in a race condition where we
+        // missed a an op, just subscribe to the specific doc again
         doc.subscribe();
       } else {
-        doc.subscribed = true; // Set before setWantSubscribe() so flush doesn't send a subscribe request.
-        doc._setWantSubscribe(true); // this will call any subscribe callbacks or whatever.
+        // Fake out a doc subscription, since we are already up to date
+        doc.subscribed = true;
+        doc.wantSubscribe = true;
+        doc.flush();
         doc.emit('subscribe');
-        doc._finishSub(); // this doesn't actually do anything here, but its more correct to have it.
+        doc._finishSub();
       }
     }
     results.push(doc);
   }
-  this.connection.bsEnd();
   return results;
 };
 

k-archive/k-share

Conflicts:
	lib/client/query.js

lib/client/query.js
@@ -67,16 +67,13 @@ Query.prototype._execute = function() {
     // Collect the version of all the documents in the current result set so we
     // don't need to be sent their snapshots again.
     var existing = this.connection.getExisting(this.collection);
+
     if (existing)
       for (var i in existing) {
         if (existing.hasOwnProperty(i)) { 
           var doc = existing[i];
-          // If we're subscribed, the server already knows which version of the doc
-          // we have.
-          if (!doc.subscribed && doc.action !== 'subscribe') {
-            var c = collectionVersions[doc.collection] = collectionVersions[doc.collection] || {};
-            c[doc.name] = doc.version;
-          }
+          var c = collectionVersions[doc.collection] = collectionVersions[doc.collection] || {};
+          c[doc.name] = doc.version;
         }
       }
   }
@@ -106,7 +103,7 @@ Query.prototype._execute = function() {
 Query.prototype._dataToDocs = function(data) {
   var results = [];
   var lastType;
-  this.connection.bsStart();
+
   for (var i = 0; i < data.length; i++) {
     var docData = data[i];
 
@@ -118,11 +115,24 @@ Query.prototype._dataToDocs = function(data) {
     }
 
     var doc = this.connection.get(docData.c || this.collection, docData.d, docData);
-    doc._finishQuerySubscribe();
+
+    // Force the document to know its subscribed if we're in docmode: 'sub'
+    if (this.docMode === 'sub') {
+        if (docData.v > doc.version) {
+          // This generally shouldn't happen, but in a race condition where we
+          // missed a an op, just subscribe to the specific doc again
+        doc.subscribe();
+      }
+      else {
+        doc._finishQuerySubscribe();
+      }
+    }
 
     results.push(doc);
   }
 
+  // we need to sort the array if this was an id query
+  // the result needs to be in the same order as the id array
   if (this.query && this.query._id && this.query._id.$in) {
     var results2 = [];
 
@@ -140,7 +150,6 @@ Query.prototype._dataToDocs = function(data) {
     results = results2;
   }
 
-  this.connection.bsEnd();
   return results;
 };
 